ab1d36413e594aafe334aa61b68aae0bd26fd5ae
Jhostyn <jhostyndavid44@gmail.com>
2026-09-14 10:52:08 -0500

    cambios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.ksp)
-}
-
-ksp {
-    arg("ksp.use.ksp2", "false")
 }
 
 android {
@@ -75,7 +70,6 @@ dependencies {
     // Room
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
-    ksp(libs.androidx.room.compiler)
 
     // DataStore
     implementation(libs.androidx.datastore.preferences)
